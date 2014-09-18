4fdf68b6f8e2b2b7ea00df9269069625d90dfa00
Ashwani Bhargava <ashwanibhrgv@gmail.com>
2014-09-18 23:05:41 +0530

    Initial commit

---
+++ Piccus/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Piccus
+//
+//  Created by Ashwani on 16/09/14.
+//  Copyright (c) 2014 Ashwani Bhargava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PiccusTests/PiccusTests.m
@@ -0,0 +1,34 @@
+//
+//  PiccusTests.m
+//  PiccusTests
+//
+//  Created by Ashwani on 16/09/14.
+//  Copyright (c) 2014 Ashwani Bhargava. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PiccusTests : XCTestCase
+
+@end
+
+@implementation PiccusTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
